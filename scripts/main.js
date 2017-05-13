b67501cb1d13ab353e4f9f270f72f08f9774d876
Jackhay98 <2haywj72@solent.ac.uk>
2017-05-13 15:18:09 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+ function main() {
+
+    console.log("in main function");
+    var myForm  = document.getElementById("pizzaOrder");
+    myForm.addEventListener("submit",validateForm);
+  }
+
+
+
+  
+  
